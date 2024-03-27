ffcd1c6c82596a31ea11d9f0a5aaff66579d972b
minho727 <072756@snu.ac.kr>
2024-03-27 10:55:06 +0900

    week9-bfs

---
+++ BFS/9주차-199-BinaryTreeRightSideView.py
@@ -0,0 +1,26 @@
+from typing import Optional, List
+import collections
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if root is None:
+            return []
+
+        rslt = []
+        queue = collections.deque([root])
+        while queue:
+            node = None
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            rslt.append(node.val)
+        return rslt
