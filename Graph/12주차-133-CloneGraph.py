18e4e064adff2dc2bc2bd5a2a35ee28bfea4db54
minho727 <072756@snu.ac.kr>
2024-05-08 16:37:23 +0900

    week12-graph

--- Graph/12주차-133-CloneGraph.py
+++ Graph/12주차-133-CloneGraph.py
@@ -3,7 +3,7 @@ class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-
+        
 # fail
 from typing import Optional
 class Solution:
