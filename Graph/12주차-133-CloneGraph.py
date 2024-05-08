9aaf7c28ec4ab98a8c3c3dba530c6230f337e1ed
minho727 <072756@snu.ac.kr>
2024-05-08 16:36:50 +0900

    week12

---
+++ Graph/12주차-133-CloneGraph.py
@@ -0,0 +1,37 @@
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+# fail
+from typing import Optional
+class Solution:
+    visited = set()
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node is None or node.val in self.visited:
+            return []
+        new = Node(node.val, node.neighbors)
+        self.visited.add(new.val)
+        for adj in new.neighbors:
+            self.cloneGraph(adj)
+        return new
+    
+## Solution
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if node is None:
+            return node
+        copied = {}
+        
+        def dfs(node):
+            if node in copied:
+                return copied[node]
+            new = Node(node.val)
+            copied[node] = new
+            for neighbor in node.neighbors:
+                new.neighbors.append(dfs(neighbor))
+            return new
+        
+        return dfs(node)
